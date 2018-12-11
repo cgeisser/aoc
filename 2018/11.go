ec792b4b2915ead149a74d984b8c3602c14510cd
Carla Geisser <cgeisser@users.noreply.github.com>
2018-12-11 22:35:24 +0000

    Part2

--- 2018/11.go
+++ 2018/11.go
@@ -10,6 +10,7 @@ import (
 
 type point struct {
 	x, y  int
+	dim   int
 	power int
 }
 
@@ -24,15 +25,25 @@ func getMax(in <-chan point, out chan<- point) {
 	close(out)
 }
 
-func sumGrid(magic int, i point, out chan<- point) {
-	//fmt.Println("thread processing: ", i)
-	for gx := i.x; gx < i.x + 3; gx++ {
-		for gy := i.y; gy < i.y + 3; gy++ {			
-			i.power += gridPower(magic, gx, gy)
+const gridmax = 300
+
+func sumGrid(grid [gridmax][gridmax]int, i point, out chan<- point) {
+	for ; i.dim <= gridmax && i.x+i.dim-1 <= gridmax && i.y+i.dim-1 <= gridmax; i.dim++ {
+		//fmt.Println("thread processing: ", i)
+
+		for gx := i.x; gx < i.x+i.dim; gx++ {
+			//	fmt.Println(gx, i.y+i.dim - 1)
+			i.power += grid[gx-1][i.y+i.dim-2]
 		}
+		for gy := i.y; gy < i.y+i.dim-1; gy++ {
+			//	fmt.Println(i.x+i.dim - 1, gy)
+			i.power += grid[i.x+i.dim-2][gy-1]
+		}
+
+		//fmt.Println("thread done processing: ", i)
+		out <- i
 	}
-	//fmt.Println("thread done processing: ", i)
-	out <- i
+
 }
 
 func gridPower(magic int, gx, gy int) int {
@@ -44,34 +55,45 @@ func gridPower(magic int, gx, gy int) int {
 	return h - 5
 }
 
-const gridmax = 300
-
 func main() {
 	fmt.Println(gridPower(8, 3, 5))
 	fmt.Println(gridPower(57, 122, 79))
 	fmt.Println(gridPower(39, 217, 196))
 	fmt.Println(gridPower(71, 101, 153))
-	
+
 	f, _ := os.Open("11.data")
 	input := bufio.NewScanner(f)
 
 	for input.Scan() {
 		magic, _ := strconv.Atoi(input.Text())
 
-		gather := make(chan point)
+		gather := make(chan point, gridmax*gridmax)
 		findMax := make(chan point)
 		var wg sync.WaitGroup
 		go getMax(gather, findMax)
-		for x := 1; x <= gridmax-2; x++ {
-			for y := 1; y <= gridmax-2; y++ {
+		var grid [gridmax][gridmax]int
+		// build grid
+		for x := range grid {
+			for y := range grid[x] {
+				grid[x][y] = gridPower(magic, x+1, y+1)
+			}
+		}
+
+		// search
+		maxthreads := make(chan struct{}, gridmax)
+		for x := 1; x <= gridmax; x++ {
+			for y := 1; y <= gridmax; y++ {
 				wg.Add(1)
-				p := point{x, y, 0}
+				p := point{x, y, 1, 0}
+				maxthreads <- struct{}{}
 				go func() {
 					defer wg.Done()
-					sumGrid(magic, p, gather)
+					sumGrid(grid, p, gather)
+					<-maxthreads
 				}()
 			}
 		}
+
 		// closer
 		go func() {
 			wg.Wait()
@@ -80,6 +102,5 @@ func main() {
 
 		answer := <-findMax
 		fmt.Printf("magic: %v answer: %v\n", magic, answer)
-		//break
 	}
 }
