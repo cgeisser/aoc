25a9567f690f4583b39dd3b2e28687fbaf4ae7cb
Carla Geisser <cgeisser@users.noreply.github.com>
2018-12-11 02:55:09 +0000

    part 2

--- 2018/2.go
+++ 2018/2.go
@@ -23,8 +23,8 @@ func main() {
 	f.Close()
 
 	var counthist = make(map[int]int)
-
-	for _, val := range values {
+	var match, match2 string
+	for loc, val := range values {
 		fmt.Println("input: ", val)
 		var stringhist = make(map[rune]int)
 		for _, c := range val {
@@ -39,8 +39,27 @@ func main() {
 		for c := range invert {
 			counthist[c]++
 		}
+		// do search for part 2
+		if match == "" && match2 == "" {
+			for _, m := range values[loc:] {
+				var diffs = 0
+				for i := range m {
+					if val[i] != m[i] {
+						diffs++
+					}
+					if diffs > 1 {
+						break
+					}
+				}
+				if diffs == 1 {
+					match = m
+					match2 = val
+				}
+			}
+		}
 	}
 	fmt.Println("twos: %v threes: %v", counthist[2], counthist[3])
 	fmt.Println(counthist[2] * counthist[3])
-
+	fmt.Println(match)
+	fmt.Println(match2)
 }
