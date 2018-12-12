846c9f57bc9548a513dde5526f77a9902ea7d1be
Carla Geisser <cgeisser@users.noreply.github.com>
2018-12-12 23:38:00 +0000

    Part 2, find a pattern with help from reddit

--- 2018/12.go
+++ 2018/12.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
-//	"bytes"
+	//	"bytes"
 	"fmt"
 	"os"
-//	"strconv"
+	//	"strconv"
 	"strings"
 )
 
@@ -24,61 +24,60 @@ func main() {
 				trans[tokens[0]] = tokens[2][0]
 			}
 		}
-	}		
+	}
 	zero_loc := 0
-	
+
 	fmt.Println("start state: ", start)
 	fmt.Println("transforms: ", trans)
 
 	orig_start := start
-	for i:=0; i<20; i++ {
+	for i := 0; i < 2000; i++ {
 		start, zero_loc = generate(start, zero_loc, trans)
-		fmt.Println(start,zero_loc)
-	}
-	
-	sum := 0
-	for i, v := range start {
-		if v == '#' {
-			sum += i-zero_loc
+		//fmt.Println(start,zero_loc)
+
+		sum := 0
+		for i, v := range start {
+			if v == '#' {
+				sum += i - zero_loc
+			}
 		}
+		fmt.Printf("sum of remaining plants @ %v: %v\n", i, sum)
 	}
-	fmt.Println("sum of remaining plants @ 20: ", sum)
 
 	// Part 2 ??
 	zero_loc = 0
 	start = orig_start
-	for i:=0; ; i++ {
+	for i := 0; ; i++ {
 		start, zero_loc = generate(start, zero_loc, trans)
-		if (i % 1000 == 0) {
+		if i%1000 == 0 {
 			fmt.Printf("%v ", i)
 		}
-		if strings.Contains(start, orig_start) {			
+		if strings.Contains(start, orig_start) {
 			fmt.Println("cycle at %v %v %v\n", i, start, orig_start)
 		}
 	}
 }
 
-
 func generate(start string, zero int, trans map[string]byte) (string, int) {
-	
+
 	if start[0:4] != "...." {
 		start = "...." + start
 		zero += 4
 	}
-	if start[len(start) - 5:len(start) - 1] != "...." {
+	if start[len(start)-5:len(start)-1] != "...." {
 		start = start + "...."
 	}
-	
+
 	var next []byte = make([]byte, 0, len(start))
 	next = append([]byte(start[0:2]))
 
-	for i := 2; i < len(start) - 2; i++ {
+	for i := 2; i < len(start)-2; i++ {
 		if trans[start[i-2:i+3]] != 0 {
 			next = append(next, trans[start[i-2:i+3]])
 		} else {
 			next = append(next, '.')
 		}
-		
+
 	}
 	return string(next), zero
 }
