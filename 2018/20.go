b280fb25c9b7d7f39b6b0080af35d9d7704e65bd
Carla Geisser <cgeisser@users.noreply.github.com>
2018-12-21 04:42:48 +0000

    hmm

---
+++ 2018/20.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type room struct {
+	xy   coord
+	dist int
+	ar   *allrooms
+	v    bool
+}
+
+type dir int
+
+const (
+	N = iota
+	E
+	S
+	W
+)
+
+func index(d byte) dir {
+	switch d {
+	case 'N':
+		return N
+	case 'E':
+		return E
+	case 'S':
+		return S
+	case 'W':
+		return W
+	default:
+		panic(fmt.Sprintf("no direction %c", d))
+	}
+}
+
+type coord [2]int
+type allrooms map[coord]*room
+
+func (d dir) moveCoord(xy coord) coord {
+	switch d {
+	case N:
+		return coord{xy[0], xy[1] - 1}
+	case E:
+		return coord{xy[0] + 1, xy[1]}
+	case S:
+		return coord{xy[0], xy[1] + 1}
+	case W:
+		return coord{xy[0] - 1, xy[1]}
+	default:
+		panic("bad direction")
+	}
+}
+
+func (cr *room) build(re string, closeblock int) int {
+	fmt.Println("  parsing: ", re, closeblock)
+	if re == "" {
+		return 0
+	}
+	if re[0] == '|' {
+		cr.build(re[closeblock:], -1)
+		return 0
+	}
+	if re[0] == ')' {
+		return cr.build(re[1:], -1) + 1
+	}
+
+	if re[0] == '(' {
+		consumed := 1
+		pcount := 0
+		closeparen := 0
+		for i, c := range re {
+			if c == '(' {
+				pcount++
+			}
+			if c == ')' {
+				pcount--
+			}
+			if c == ')' && pcount == 0 {
+				closeparen = i
+				break
+			}
+		}
+		for ; consumed < closeparen; consumed++ {
+		//	fmt.Println(" option", re[consumed:])
+			consumed += cr.build(re[consumed:], closeparen-consumed)	
+		}
+		return consumed
+	} else {
+		d := index(re[0])
+		movec := d.moveCoord(cr.xy)
+
+		nextroom := (*cr.ar)[movec]
+		if nextroom == nil {
+			nextroom = &room{movec, cr.dist + 1, cr.ar, false}
+			//fmt.Println("A new room!", nextroom)
+			(*cr.ar)[movec] = nextroom
+		} else if cr.dist+1 < nextroom.dist {
+			fmt.Println("shorter path: ", cr.dist+1)
+			nextroom.dist = cr.dist + 1
+		}
+		return nextroom.build(re[1:], closeblock-1)
+	}
+}
+
+func (r *room) maxdist() int {
+	max := 0
+	for _, n := range *r.ar {
+		if n.dist > max {
+			max = n.dist
+		}
+	}
+	return max
+}
+
+func main() {
+	f, _ := os.Open("20.data")
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		if input.Text()[0] == '#' {
+			continue
+		}
+		if input.Text()[0] != '^' || input.Text()[len(input.Text())-1] != '$' {
+			panic("bad row")
+		}
+
+		r := &room{}
+		r.ar = &allrooms{ r.xy:r }
+		fmt.Println("running ",input.Text())
+		r.build(input.Text()[1 : len(input.Text())-1], -1)
+		for x, v := range *r.ar {
+			fmt.Println(x, v)
+		}
+		fmt.Println(" max dist:", r.maxdist())
+		//	break
+	}
+
+}
